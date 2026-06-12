0ea5601e711c6774459879f8147e716a9ce4b82a
Yosua <yhssua@gmail..com>
2026-06-12 11:05:34 +0700

    Switch realtime broadcasting to Pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -4,11 +4,10 @@ import Pusher from "pusher-js";
 window.Pusher = Pusher;
 
 window.Echo = new Echo({
-    broadcaster: "reverb",
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: false,
-    enabledTransports: ["ws", "wss"],
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
 });
+
+console.log("Echo Pusher loaded:", window.Echo);
